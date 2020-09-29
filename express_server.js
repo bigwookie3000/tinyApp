372264f1f8043c91ade98413beda9eade68f3460
Ryan Williams <bigwookie3000@gmail.com>
2020-09-29 01:21:26 +0000

    day 1 commit

--- express_server.js
+++ express_server.js
@@ -18,6 +18,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.body.longURL };
+  res.render("urls_show", templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
